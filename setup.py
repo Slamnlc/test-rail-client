f13f90d94e30e433b308569110c6f3331d60d343
m.biriukov <m.biriukov@Elsevier.com>
2021-10-04 16:05:46 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='testrail-api-client',
+    version='0.1',
+    use_scm_version=False,
+    description='TestRail Api Python Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Biriukov Maksym',
+    author_email='maximbirukov77@gmail.com',
+    url="https://github.com/Slamnlc/test-rail-client",
+    download_url='https://github.com/Slamnlc/test-rail-client/archive/refs/tags/v0.1.tar.gz',
+    packages=setuptools.find_packages(exclude=("tests", "dev_tools")),
+    install_requires=[
+        'requests'
+    ],
+    entry_points={
+        "test_rail_api": [
+            "test_rail_api = api_client",
+        ]
+    },
+)
